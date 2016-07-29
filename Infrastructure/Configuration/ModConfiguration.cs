eafa1fcf70705cea1cbb3ff94effc4e65864c845
Asbjørn Hansen <ah@fenris.io>
2016-07-29 23:36:27 +0200

    added infrastructure project from birdcage to the solution

---
+++ Infrastructure/Configuration/ModConfiguration.cs
@@ -0,0 +1,15 @@
+﻿namespace SexyFishHorse.CitiesSkylines.Infrastructure.Configuration
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class ModConfiguration
+    {
+        public ModConfiguration()
+        {
+            Settings = new List<KeyValuePair<string, object>>();
+        }
+
+        public List<KeyValuePair<string, object>> Settings { get; set; }
+    }
+}
